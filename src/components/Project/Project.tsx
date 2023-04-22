93d8002cb205dd5b34b94a59338cfa592e611241
nijuusan23 <104360488+nijuusan23@users.noreply.github.com>
2023-04-22 11:42:36 +0800

    add overflow in project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -12,7 +12,7 @@ function Project({ project }: ProjectProps) {
         className="object-cover w-full h-full rounded-lg"
         src={project.image?.url}
       />
-      <div className="text-center p-5 absolute inset-0 flex items-center justify-center backdrop-blur-lg backdrop-filter opacity-0 transition-opacity duration-300 rounded-lg">
+      <div className="text-center p-5 absolute inset-0 flex items-center justify-center backdrop-blur-lg backdrop-filter opacity-0 transition-opacity duration-300 rounded-lg overflow-auto	mt-0 sm:mt-5 mb sm:mb-5">
         <div>
           <div className="text-amber-300 text-2xl font-bold">
             {project.name}
