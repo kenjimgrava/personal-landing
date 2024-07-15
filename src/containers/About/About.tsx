03c90b93a41664a08403a27f933dd9dc77f94817
Kenji Mille <jinek.llim@Kenjis-MacBook-Pro.local>
2024-07-15 17:22:41 +0800

    modify about me

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -26,14 +26,22 @@ function About() {
             and I'm{" "}
             <span className="text-color-blue text-2xl">Software Developer</span>
           </div>
-          <Button text="Download my Resume" callBack={downloadResume} className="transform transition-transform hover:scale-105"/>
+          <Button
+            text="Download my Resume"
+            callBack={downloadResume}
+            className="transform transition-transform hover:scale-105"
+          />
         </div>
         <div className="p-5">
           <img src={image} width={700} className="rounded-lg" />
           <div className="flex gap-4 -mt-14 justify-between ">
             <div></div>
             <div className="flex">
-              <a href="https://www.linkedin.com/in/kmg-4all" target="_blank" className="transform transition-transform hover:scale-105">
+              <a
+                href="https://www.linkedin.com/in/kmg-4all"
+                target="_blank"
+                className="transform transition-transform hover:scale-105"
+              >
                 <img
                   className="cursor-pointer rounded-lg"
                   src={linkedIn}
@@ -62,30 +70,12 @@ function About() {
         <div className="flex  gap-5">
           <div className="border h-1  border-amber-300 w-20"></div>
           <div className="-mt-2 text-xl">
-            I have <span className="text-color-blue">{new Date().getFullYear()- 2019}-{ new Date().getFullYear()+1 - 2019} years</span> of
-            development experience and I am{" "}
-            <span className="text-color-blue">passionate</span> about{" "}
-            <span className="text-color-blue">learning</span> and{" "}
-            <span className="text-color-blue">expanding my skillset</span>, and
-            I thrive in{" "}
-            <span className="text-color-blue">challenging environments</span>{" "}
-            that allow me to{" "}
-            <span className="text-color-blue">collaborate</span> with other{" "}
-            <span className="text-color-blue">talented individuals</span> and{" "}
-            <span className="text-color-blue">contribute</span> to{" "}
-            <span className="text-color-blue">innovative projects</span>. As a{" "}
-            <span className="text-color-blue">software developer</span>, I am
-            always looking for new ways to improve and{" "}
-            <span className="text-color-blue">streamline processes</span>, and I
-            am committed to{" "}
-            <span className="text-color-blue">producing high-quality work</span>{" "}
-            that meets the needs of both <span className="text-color-blue">clients</span> and <span className="text-color-blue">end-users</span>. If you are
-            looking for a dedicated and{" "}
-            <span className="text-color-blue">skilled software developer </span>
-            to join your team, I would love to talk with you about how my{" "}
-            <span className="text-color-blue">experience</span> and{" "}
-            <span className="text-color-blue">expertise</span> can benefit your
-            organization. Thank you and have a nice day!
+            I'm an experienced Software Developer with 5 - 6 years of
+            experience, specializing in web applications. My expertise includes
+            React, TypeScript, JavaScript, and UI libraries. I focus on
+            delivering pixel-perfect, responsive designs and have skills in
+            optimizing performance, conducting testing, and collaborating within
+            cross- functional teams.
           </div>
         </div>
       </div>
